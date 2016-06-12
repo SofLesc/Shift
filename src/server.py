8eb0adf5653491aa762099ba6b3bd53a7ab854d8
Victor R. Cardoso <victor.roth.cardoso@gmail.com>
2016-06-12 11:16:37 -0300

    tests

--- src/server.py
+++ src/server.py
@@ -126,10 +126,12 @@ def logged_page():
     #    return ret
     #    return "logado", request.args['first_name'], request.args['last_name']
 
-    first_replace = '<img class="user" id="profile-img" alt="Buy with MasterPass" src="https://graph.facebook.com/' + request.args['id'] + '/picture?type="large"/>'
-    second_replace = '<h3 class="section-title" id="place">' + str(place_name) + '</h3><h4 class="section-title" id="price">' + str(price) + '</h4>'
+    first_replace = '<img class="user" id="profile-img" alt="Buy with MasterPass" src="https://graph.facebook.com/' + str(request.args['id']) + '/picture?type="large"/>'
+    second_replace = '<h3 class="section-title" id="place">' + str(place_name) + '</h3><h4 class="section-title" id="price">$' + str(price) + '</h4>'
     _part = data_logged_page.replace("$$FIRST$$", first_replace)
+    print(_part)
     _part = _part.replace("$$SECOND$$", second_replace)
+    print(_part)
     return _part
 
 
