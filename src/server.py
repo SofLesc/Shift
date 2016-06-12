ef2f854a20d3968589bc2d78a2a8df7fe2164bde
Victor R. Cardoso <victor.roth.cardoso@gmail.com>
2016-06-12 07:21:20 -0300

    melhorando csss phot ...

--- src/server.py
+++ src/server.py
@@ -110,7 +110,7 @@ def logged_page():
     #    return ret
     #    return "logado", request.args['first_name'], request.args['last_name']
 
-    return data_logged_page.replace("$$FIRST$$", '<img class="user" id="profile-img" alt="Buy with MasterPass" src="https://graph.facebook.com/'+ request.args['id'] +'/picture?type=large" /> <h3 class="section-title" id="place">'+ str(place_name) +'</h3><h4 class="section-title" id="price">'+ str(price) +"$</h4>")
+    return data_logged_page.replace("$$FIRST$$", '<img class="user" id="profile-img" alt="Buy with MasterPass" src="https://graph.facebook.com/'+ request.args['id'] +'/picture?type=large" />').replace("$$SECOND$$", '<h3 class="section-title" id="place">'+ str(place_name) +'</h3><h4 class="section-title" id="price">'+ str(price) +'$</h4>')
 
 
 
